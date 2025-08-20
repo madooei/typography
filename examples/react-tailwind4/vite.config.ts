9191bd0e0acc28856588eca16977c27531efba8c
Ali Madooei <alimadooei@gmail.com>
2025-08-20 11:33:35 -0400

    fix: add server configuration to Vite examples and include adjust-size folder in workspace

--- examples/react-tailwind4/vite.config.ts
+++ examples/react-tailwind4/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    server: {
+  server: {
     port: 4002,
     open: true,
   },
